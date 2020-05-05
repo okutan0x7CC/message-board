6ffdbdee4bd94cf5d59126563f6c4cedae1657e2
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-05-06 07:48:19 +0900

    [add] admin のデプロイ対象にstaging を追加し、デプロイコマンドを作成

--- admin/deploy.sh
+++ admin/deploy.sh
@@ -1,4 +1,30 @@
+#!/bin/bash
+
+echo "\nPlease select a deployment destination.\n"
+PS3="Enter a name: "
+destination=""
+select env in staging production; do
+    if [ -n "$REPLY" ]; then
+        if [ $REPLY = "staging" ]; then
+            destination="staging"
+        elif [ $REPLY = "production" ]; then
+            destination="production"
+        else
+            echo "Invalid!"
+            continue
+        fi
+    else
+        echo "Invalid!"
+        continue
+    fi
+    break
+done
+
+echo "\n🔥 Start deploying $destination !\n"
+
 rm -rf ./dist && rm -rf ./functions/admin/*
 npm run build
 mv ./dist/index.html ./functions/admin/index.html
-firebase deploy
+firebase deploy --project=$destination --only hosting:$destination,functions
+
+echo "\n🍺 End deployment!\n"
