9f9598169c03bd53b875da3b58fcac59c238871a
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-05-06 09:43:58 +0900

    [add] デプロイコマンドを新構成用に再作成

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,63 @@
 #!/bin/bash
 
-echo "\nPlease select a deployment destination.\n"
-PS3="Enter a name: "
-destination=""
-select env in staging production; do
-    if [ -n "$REPLY" ]; then
-        if [ $REPLY = "staging" ]; then
-            destination="staging"
-        elif [ $REPLY = "production" ]; then
-            destination="production"
-        else
-            echo "Invalid!"
+printf "\nPlease select a deployment destination.\n"
+PS3="Enter a number: "
+select env in staging production quit; do
+    case $env in
+        staging)
+            printf "\n⭐️ Selected destination: $env ⭐️\n"
+            ;;
+        production)
+            printf "\n🚨 Selected destination: $env 🚨\n"
+            ;;
+        quit)
+            exit
+            ;;
+        *)
+            printf "Invalid option $REPLY\n"
             continue
-        fi
-    else
-        echo "Invalid!"
-        continue
-    fi
+            ;;
+    esac
+
+    printf "\nPlease select a target.\n"
+    PS3="Enter a number: "
+    select target in admin_and_functions client projection database; do
+        case $target in
+            admin_and_functions)
+                printf "Target: $target"
+                rm -rf ./admin/dist && rm -rf ./functions/admin/*
+                cd admin && npm run build && cd ..
+                mv ./admin/dist/index.html ./functions/admin/index.html
+                firebase deploy --project=$env --only hosting:admin,functions
+                ;;
+            client)
+                printf "Target: $target"
+                rm -rf ./client/dist
+                cd client && npm run build && cd ..
+                firebase deploy --project=$env --only hosting:client
+                ;;
+            projection)
+                printf "Target: $target"
+                rm -rf ./projection/dist
+                cd projection && npm run build && cd ..
+                firebase deploy --project=$env --only hosting:projection
+                ;;
+            database)
+                printf "Target: $target"
+                firebase deploy --project=$env --only database
+                ;;
+            quit)
+                exit
+                ;;
+            *)
+                printf "Invalid option $REPLY\n"
+                continue
+                ;;
+        esac
+        break
+    done
+
     break
 done
 
-echo "\n🔥 Start deploying $destination !\n"
-
-rm -rf ./dist && rm -rf ./functions/admin/*
-npm run build
-mv ./dist/index.html ./functions/admin/index.html
-firebase deploy --project=$destination --only hosting:$destination,functions
-
-echo "\n🍺 End deployment!\n"
+printf "\n🍺 End deployment!\n"
