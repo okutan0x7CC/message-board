2902dcbcfaeefe405867e48bd804efe4c9e856a9
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-05-10 20:52:30 +0900

    [update] 微修正

--- generate_env.py
+++ generate_env.py
@@ -9,12 +9,11 @@ os.system(f"firebase functions:config:get > {file_name}")
 with open(file_name) as file:
     env_data = json.load(file)["env"]
 
-print("tmp.env.json has been created.\n.env generating...")
+print(f"{file_name} has been created.\n.env generating...")
 
 env_content = "# This .env is automatically generated.\n# If you want to change it, please refer to the following.\n# https://firebase.google.com/docs/functions/config-env\n\n"
 for key, value in env_data.items():
     env_content += f"{key.upper()}={value}\n"
-
 
 target_directory = ["admin", "client", "database", "projection"]
 for directory in target_directory:
