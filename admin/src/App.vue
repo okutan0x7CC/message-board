575b5564ac32c5cc16a87c907f4bf506cf6efe9c
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-06-21 13:01:48 +0900

    [change] 各プロジェクトの env は隠蔽することがそもそも不可能であるため、自動生成から除外する

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -19,14 +19,14 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAt5Sf9hP6EQ4p_6LDiwAl6c_iCIoZTBEQ",
-  authDomain: "stg-message-board.firebaseapp.com",
-  databaseURL: "https://stg-message-board.firebaseio.com",
-  projectId: "stg-message-board",
-  storageBucket: "stg-message-board.appspot.com",
-  messagingSenderId: "566183247774",
-  appId: "1:566183247774:web:f4752cc97196683a81a08f",
-  measurementId: "G-TB5Y7WCYRP"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
