ce354d102ca61c740ace99651b99cdbc99cb6593
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-06-22 15:59:57 +0900

    [add] room create function

--- admin/src/components/RoomCreate.vue
+++ admin/src/components/RoomCreate.vue
@@ -1,12 +1,56 @@
 <template>
   <div>
-    hoge
+    <div>
+      <label for="public-title">public title: </label>
+      <input v-model="public_title" type="text" id="public-title" />
+    </div>
+    <div>
+      <label for="private-title">private title: </label>
+      <input v-model="private_title" type="text" id="private-title" />
+    </div>
+    <div>
+      <label for="can-read">can read: </label>
+      <input v-model="can_read" type="checkbox" id="can-read" />
+    </div>
+    <div>
+      <label for="can-write">can write: </label>
+      <input v-model="can_write" type="checkbox" id="can-write" />
+    </div>
+    <div>
+      <button v-on:click="createRoom()">crete</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { db } from "./../App.vue";
+
 export default {
   name: "RoomCreate",
+  data: function() {
+    return {
+      public_title: "",
+      private_title: "",
+      can_read: false,
+      can_write: false,
+    };
+  },
+  methods: {
+    createRoom: function() {
+      const self = this;
+      db.ref("rooms").push(
+        {
+          public_title: self.public_title,
+          private_title: self.private_title,
+          can_read: self.can_read,
+          can_write: self.can_write,
+        },
+        () => {
+          self.$router.back();
+        }
+      );
+    },
+  },
 };
 </script>
 
