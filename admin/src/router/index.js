adcf960aa4ab915fc11d714f673a05e0b08dc20a
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-06-22 11:56:23 +0900

    [add] not found page

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,21 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import RoomList from "./../components/RoomList.vue";
+import NotFound from "./../errors/NotFound.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "RoomList",
-        component: RoomList,
-    },
+  {
+    path: "/",
+    name: "RoomList",
+    component: RoomList,
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
+  },
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 
 export default router;
