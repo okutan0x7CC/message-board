2749b2b84a934654f5b842030aa74bd357e8a0e7
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-07-05 21:02:13 +0900

    [update] z-index: 1 だと datatable の検索アイコンと被ってしまうので、10 にした

--- admin/src/components/TheNavigationBar.vue
+++ admin/src/components/TheNavigationBar.vue
@@ -3,16 +3,12 @@
     <i-navbar-brand :to="{ name: 'RoomList' }">MessageBoard</i-navbar-brand>
     <i-navbar-items>
       <i-nav>
-        <i-nav-item :to="{ name: 'contact' }">AdminAccounts</i-nav-item>
+        <i-nav-item :to="{ name: 'AdminUserList' }">AdminAccounts</i-nav-item>
       </i-nav>
       <i-nav>
         <i-dropdown placement="bottom-end">
           <i-button variant="primary" link>
-            <img
-              v-if="login_user.photo_url"
-              :src="login_user.photo_url"
-              alt="icon"
-            />
+            <img v-if="login_user.photo_url" :src="login_user.photo_url" alt="icon" />
           </i-button>
           <i-dropdown-menu>
             <i-dropdown-item class="_padding-left-1-2 _padding-right-1-2">
@@ -21,9 +17,7 @@
               </i-button>
             </i-dropdown-item>
             <i-dropdown-item class="_padding-left-1-2 _padding-right-1-2">
-              <i-button class="_width-100" v-on:click="logout">
-                logout
-              </i-button>
+              <i-button class="_width-100" v-on:click="logout">logout</i-button>
             </i-dropdown-item>
           </i-dropdown-menu>
         </i-dropdown>
@@ -38,7 +32,7 @@ import { auth } from "./../main.js";
 export default {
   name: "TheNavigationBar",
   props: {
-    login_user: Object,
+    login_user: Object
   },
   methods: {
     logout: function() {
@@ -47,15 +41,15 @@ export default {
     toggleDarkMode() {
       this.$inkline.config.variant =
         this.$inkline.config.variant === "light" ? "dark" : "light";
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .navbar {
   // navbar に z-index を入れない場合、dropdown が下の DOM 要素に隠れてしまう
-  z-index: 1;
+  z-index: 10;
 }
 img {
   height: 36px;
