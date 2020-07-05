0d115619333eaacd710d5562e3973a135c78a622
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-07-05 14:13:40 +0900

    [add] inkline の custom scss に対応

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,6 +1,16 @@
+const path = require("path");
 module.exports = {
+  transpileDependencies: ["@inkline/inkline"],
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [
+        path.resolve(__dirname, "src/css/inkline/config/index.scss"),
+        path.resolve(__dirname, "src/assets/variables.scss"),
+      ],
+    },
+  },
   configureWebpack: {
     devtool: "source-map",
   },
-  transpileDependencies: ["@inkline/inkline"],
 };
