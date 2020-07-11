f2df9b1818657221ce4f4defd4a9297b1ced7663
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-07-12 00:21:41 +0900

    [add] logger の log level で console.log の表示結果を変える

--- admin/src/plugins/logger.js
+++ admin/src/plugins/logger.js
@@ -1,23 +1,29 @@
 export default {
   install(Vue) {
     const is_production = process.env.NODE_ENV === "production";
-    function log(type, title, text) {
+    function log(type, title, text, background, color = "white") {
       if (!is_production) {
-        console.log(`[${type}]: ${title}\n${text}`);
+        console.log(
+          `%c ${type} ` + `%c${title}\n${text}`,
+          `background: ${background}; color: ${color};`
+        );
       }
     }
     Vue.prototype.$log = {
       error(title, text) {
-        log("ERROR", title, text);
+        log("ERROR", title, text, "red");
       },
       warning(title, text) {
-        log("WARNING", title, text);
+        log("WARNING", title, text, "yellow", "black");
+      },
+      succeed(title, text) {
+        log("SUCCEED", title, text, "green");
       },
       info(title, text) {
-        log("INFO", title, text);
+        log("INFO", title, text, "cyan", "black");
       },
       debug(title, text) {
-        log("DEBUG", title, text);
+        log("DEBUG", title, text, "blue");
       },
     };
   },
