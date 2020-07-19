a174fd6cc58b2cbc3f9682a615e07c45146c5abe
okutan0x7CC <32217053+okutan0x7CC@users.noreply.github.com>
2020-07-19 12:22:10 +0900

    [change] auth process に store を導入

--- admin/src/components/AuthProcess.vue
+++ admin/src/components/AuthProcess.vue
@@ -4,25 +4,25 @@
       <i-container>
         <i-row center-xs class="_vh-100">
           <i-column class="_margin-auto">
-            <div v-if="is_authenticating">
+            <div v-if="shared_state.authentication_status.in_process">
               <i-loader size="sm" />
               <div>認証中</div>
             </div>
-            <div v-else-if="is_authentication_failure">
+            <div v-else-if="shared_state.authentication_status.failure">
               <p class="_font-weight-bolder">認証失敗</p>
               <p>
                 認証処理に不具合が生じました。
                 <br />ページをリロードするか再度ログインしてください。
               </p>
-              <i-button v-on:click="relogin()">relogin</i-button>
+              <i-button v-on:click="reLogin()">reLogin</i-button>
             </div>
-            <div v-else-if="!can_read_by_login_user">
+            <div v-else-if="!shared_state.login_user.can_read">
               <p class="_font-weight-bolder">権限がありません</p>
               <p>
                 本システムの操作に必要な権限が付与されておりません。
                 <br />管理者に問い合わせるか、権限を有している別アカウントで再度ログインしてください。
               </p>
-              <i-button v-on:click="relogin()">relogin</i-button>
+              <i-button v-on:click="reLogin()">reLogin</i-button>
             </div>
           </i-column>
         </i-row>
@@ -33,16 +33,17 @@
 
 <script>
 import { auth } from "./../main.js";
+import { store } from "./../store/store.js";
 
 export default {
   name: "AuthProcess",
-  props: {
-    is_authenticating: Boolean,
-    is_authentication_failure: Boolean,
-    can_read_by_login_user: Boolean
+  data() {
+    return {
+      shared_state: store.state
+    };
   },
   methods: {
-    relogin: function() {
+    reLogin() {
       auth.signOut().finally(() => {
         self.googleLogin();
       });
